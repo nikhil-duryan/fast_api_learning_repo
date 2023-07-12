b48010f5a528a750ac7f6aa6c867ba081100b07c
Nikhil <nikhil.kumar@314ecorp.com>
2023-07-12 15:39:19 +0530

    feat: understand and add response_model in fastapi

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+from typing import Any
 from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
 from auth_routes import auth_router
 from order_routes import order_router
 
 app = FastAPI()
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: str | None
+
+class UserOut(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None
+
+@app.post('/user/', response_model = UserOut)
+async def create_user(user: UserIn) -> Any:
+    return user
 
 app.include_router(auth_router)
 app.include_router(order_router)
