800d913373c0b32e139bc3d81304e78fdcd4c773
Nikhil <nikhil.kumar@314ecorp.com>
2023-07-13 13:04:12 +0530

    use different model for different things

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
-from typing import Any
 from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
-from auth_routes import auth_router
-from order_routes import order_router
 
 app = FastAPI()
 
-class UserIn(BaseModel):
+
+class UserBase(BaseModel):
     username: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+class UserIn(UserBase):
     password: str
-    email: EmailStr
-    full_name: str | None
 
-class UserOut(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: str | None
 
-@app.post('/user/', response_model = UserOut)
-async def create_user(user: UserIn) -> Any:
-    return user
+class UserOut(UserBase):
+    pass
 
-app.include_router(auth_router)
-app.include_router(order_router)
+
+class UserInDB(UserBase):
+    hashed_password: str
+
+
+def fake_password_hasher(raw_password: str):
+    return "supersecret" + raw_password
+
+
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
+
+
+@app.post("/user/", response_model=UserOut)
+async def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    return user_saved
